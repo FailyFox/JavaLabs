e6c21829cdf97cc012ee5a8e7b983979e3c5d10b
matviy <matviy2002ut@gmail.com>
2020-11-04 23:12:20 +0200

    last commit

--- JavaLab2/src/Book.java
+++ JavaLab2/src/Book.java
@@ -1,13 +1,25 @@
 public class Book {
-    public int id, year, numOfPages, price;
-    public String name, author, publisher;
+    private static int counter = 1;
+    private int id, year, numOfPages, price;
+    private String name, author, publisher;
+
+    public Book(String name, String author, String publisher, int year, int numOfPages, int price) {
+        this.id = counter;
+        this.name = name;
+        this.author = author;
+        this.publisher = publisher;
+        this.year = year;
+        this.numOfPages = numOfPages;
+        this.price = price;
+        counter++;
+    }
 
     public void setID(int ID) { id = ID; }
     public void setYear(int Year) { year = Year; }
     public void setNumOfPages(int NumOfPages) { numOfPages = NumOfPages; }
     public void setPrice(int Price) { price = Price; }
     public void setName(String Name) { name = Name; }
-    public void setAuhor(String Author) { author = Author; }
+    public void setAuthor(String Author) { author = Author; }
     public void setPublisher(String Publisher) { publisher = Publisher; }
 
     public int getID() { return id; }
--- JavaLab2/src/Main.java
+++ JavaLab2/src/Main.java
@@ -1,12 +1,69 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
+    public static Book[] randomBookArray(int count) {
+        Book[] book = new Book[count];
+        Random rand = new Random();
+        String[] authors = {"William Shakespeare", "Agatha Christie",
+                            "J. K. Rowling", "Leo Tolstoy", "Stephen King",
+                            "Alexander Pushkin", "Arthur Conan Doyle", "Victor Hugo"};
+        String[] names = {"The Lord of the Rings", "The Hobbit",
+                            "Harry Potter and the Philosopher's Stone", "Charlotte's Web",
+                            "The Tale of Peter Rabbit", "War and Peace", "Anna Karenina"};
+        String[] publishers = {"Penguin Random House", "Hachette Livre",
+                                "HarperCollins", "Macmillan Publishers",
+                                "Simon & Schuster"};
+        for(int i = 0; i < count; i++) {
+            book[i] = new Book(
+                    names[rand.nextInt(names.length)],
+                    authors[rand.nextInt(authors.length)],
+                    publishers[rand.nextInt(publishers.length)],
+                    rand.nextInt(200) + 1800,
+                    rand.nextInt(400) + 100,
+                    rand.nextInt(200) + 100);
+        }
+        return book;
+    }
+
+    public static void showListOfBookByAuthor(Book[] book, int e) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter author: ");
+        String author = scan.nextLine();
+        for(int i = 0; i < e; i++)
+            if(book[i].getAuthor().equals(author))
+                System.out.println(book[i].toString());
+    }
+    public static void showListOfBookByPublisher(Book[] book, int e) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter publisher: ");
+        String publisher = scan.nextLine();
+        for(int i = 0; i < e; i++)
+            if(book[i].getAuthor().equals(publisher))
+                System.out.println(book[i].toString());
+    }
+    public static void showListOfBookAfterYear(Book[] book, int e) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter year: ");
+        int year = scan.nextInt();
+        for(int i = 0; i < e; i++)
+            if(book[i].getYear() > year)
+                System.out.println(book[i].toString());
+    }
+
+    public static void showBooks(Book[] b) {
+        for (Book book : b) {
+            System.out.println(book.toString());
+        }
+    }
+
     public static void main(String []args) {
-        Book book = new Book();
-
-        //список книг заданого автора
-
-        //список книг, що видані заданим видавнитцтвом
-
-        //список книг, що випущені після заданого року
-
+        Scanner scan = new Scanner(System.in);
+        int count = 10;
+        Book[] book = randomBookArray(count);
+        showBooks(book);
+        showListOfBookByAuthor(book, count);
+        showListOfBookByPublisher(book, count);
+        showListOfBookAfterYear(book, count);
     }
 }
